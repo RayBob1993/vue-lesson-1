7d3703d9468be7e42e15b7cfe775163ca57dac14
RayBob1993 <dimaxfallenangel@yandex.ru>
2024-01-20 15:46:12 +0300

    пагинация

--- components/product-card.js
+++ components/product-card.js
@@ -1,12 +1,27 @@
+const { ref, computed } = Vue;
+
 export const ProductCard = {
     props: [
         'title',
         'image',
-        'price'
+        'price',
+        'images'
     ],
     template: `
         <article class="product-card">
-            <img :src="image" class="product-card__image"> 
+            <img 
+                :src="currentImage" 
+                class="product-card__image"
+            > 
+
+            <div class="product-card__images">
+                <img 
+                    v-for="image in images"
+                    :src="image"
+                    @click="onClick(image)"
+                    class="product-card__images-image"
+                >
+            </div>
 
             <h6 class="product-card__title">
                 {{ title }}
@@ -16,5 +31,20 @@ export const ProductCard = {
                 {{ price }} $
             </div>
         </article>
-    `
+    `,
+    setup (props) {
+        const newImage = ref();
+
+        const currentImage = computed(() => newImage.value || props.image);
+
+        function onClick (url) {
+            newImage.value = url;
+        }
+
+        return {
+            currentImage,
+            newImage,
+            onClick
+        }
+    }
 }
--- components/products-list.js
+++ components/products-list.js
@@ -2,24 +2,51 @@ const { ref } = Vue;
 
 export const ProductsList = {
     template: `
-        <v-product-card 
-            v-for="product in products"
-            :title="product.title"
-            :image="product.thumbnail"
-            :price="product.price"
-        />
+        <button @click="onNextPage(0)">1</button>
+        <button @click="onNextPage(9)">2</button>
+        <button @click="onNextPage(18)">3</button>
+
+        <div 
+            v-if="products.length"
+            class="products-list"
+        >
+            <v-product-card 
+                v-for="product in products"
+                :title="product.title"
+                :image="product.thumbnail"
+                :price="product.price"
+                :images="product.images"
+            />
+        </div>
+
+        <div v-else>
+            Загрузка...
+        </div>
     `,
     setup () {
         const products = ref([]);
 
-        fetch('https://dummyjson.com/products')
-        .then(res => res.json())
-        .then(function (data) {
-            products.value = data.products;
-        });
+        function getProducts (skip = 0) {
+            fetch(`https://dummyjson.com/products?limit=9&skip=${skip}`)
+            .then(res => res.json())
+            .then((data) => {
+                setTimeout(() => {
+                    products.value = data.products;
+                }, 2000);
+            });
+        }
+
+        function onNextPage (skip = 0) {
+            products.value = [];
+
+            getProducts(skip);
+        }
+
+        getProducts();
 
         return {
-            products
+            products,
+            onNextPage
         };
     }
 }
